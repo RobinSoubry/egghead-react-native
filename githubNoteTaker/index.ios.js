a1c92872314d458b69b0076fb3520b3f01cf7987
Robin Soubry <robin.soubry@gmail.com>
2016-06-24 13:34:00 -0700

    Add index page with route to main component

--- githubNoteTaker/index.ios.js
+++ githubNoteTaker/index.ios.js
@@ -9,24 +9,49 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator
 } from 'react-native';
 
+import Main from './Main.js';
+
+var NavigationBarRouteMapper = {
+  LeftButton: function( route, navigator, index, navState ){
+     return(
+       <Text>{ route.leftButton }</Text>
+     )
+   },
+   Title: function( route, navigator, index, navState ){
+     return(
+       <Text>{ route.title }</Text>
+     )
+   },
+   RightButton: function( route, navigator, index, navState ){
+     return(
+       <Text>{ route.rightButton }</Text>
+     )
+   }
+  }
+
+
 class githubNoteTaker extends Component {
+  renderScene(route, navigator){
+    if(route.title === 'Github Notetaker'){
+      return <Main navigator={navigator} />
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{title: 'Github Notetaker'}}
+        renderScene={this.renderScene.bind(this)}
+        navigationBar={
+          <Navigator.NavigationBar
+          routeMapper={ NavigationBarRouteMapper }
+          />
+        }
+      />
     );
   }
 }
@@ -34,19 +59,7 @@ class githubNoteTaker extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111',
   },
 });
 
