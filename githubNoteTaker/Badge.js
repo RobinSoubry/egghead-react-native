788f1301d1fe9f82e8d0bf2d7277122cf6934153
Robin Soubry <robin.soubry@gmail.com>
2016-06-24 19:26:02 -0700

    Add user badge with PropTypes

---
+++ githubNoteTaker/Badge.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+class Badge extends Component {
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image}/>
+        <Text style={styles.name}>{this.props.userInfo.name}</Text>
+        <Text style={styles.handle}> {this.props.userInfo.login} </Text>
+      </View>
+    )
+  }
+};
+
+// proptypes will check the available objects in userInfo. If something is not available, it will throw an error in the console.
+Badge.propTypes{
+  userInfo: React.propTypes.object.isRequired
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#48BBEC',
+    paddingBottom: 10
+  },
+  name: {
+    alignSelf: 'center',
+    fontSize: 21,
+    marginTop: 10,
+    marginBottom: 5,
+    color: 'white'
+  },
+  handle: {
+    alignSelf: 'center',
+    fontSize: 16,
+    color: 'white'
+  },
+  image: {
+    height: 125,
+    width: 125,
+    borderRadius: 65,
+    marginTop: 10,
+    alignSelf: 'center'
+  },
+});
+
+export default Badge;
