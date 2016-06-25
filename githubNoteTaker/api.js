d92f64f68b700468ada0ff67ff5d5e456628049f
Robin Soubry <robin.soubry@gmail.com>
2016-06-25 11:22:36 -0700

    Refactor api.js with comma's

--- githubNoteTaker/api.js
+++ githubNoteTaker/api.js
@@ -14,7 +14,7 @@ var api = {
     // Is the same as
     // var url = "https://api.github.com/users/" + username + "/repos"
     return fetch(url).then((response) => response.json()); // promise
-  }
+  },
 };
 
 export default api;
