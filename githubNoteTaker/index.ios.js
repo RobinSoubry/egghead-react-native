b4cde827eee414354a4a9e836ed7a50867e39c62
Robin Soubry <robin.soubry@gmail.com>
2016-06-24 22:14:42 -0700

    Update navbar to keep track of path with back button

--- githubNoteTaker/index.ios.js
+++ githubNoteTaker/index.ios.js
@@ -11,6 +11,8 @@ import {
   Text,
   View,
   Navigator,
+  TouchableHighlight,
+  TouchableOpacity,
 } from 'react-native';
 
 import Main from './Main.js';
@@ -20,22 +22,29 @@ import Badge from './Badge.js';
 
 
 var NavigationBarRouteMapper = {
-  LeftButton: function( route, navigator, index, navState ){
-     return(
-       <Text>{ route.leftButton }</Text>
-     )
-   },
-   Title: function( route, navigator, index, navState ){
-     return(
-       <Text>{ route.title }</Text>
-     )
-   },
-   RightButton: function( route, navigator, index, navState ){
-     return(
-       <Text>{ route.rightButton }</Text>
-     )
-   }
+  LeftButton(route, navigator, index, navState) {
+    if(index > 0) {
+      return (
+        <TouchableHighlight
+        	 underlayColor="transparent"
+           onPress={() => { if (index > 0) { navigator.pop() } }}>
+          <Text style={ styles.leftNavButtonText }>Back</Text>
+        </TouchableHighlight>
+  	)}
+  	else { return null }
+  },
+  RightButton(route, navigator, index, navState) {
+    if (route.onPress) return ( <TouchableHighlight
+    														onPress={ () => route.onPress() }>
+                                <Text style={ styles.rightNavButtonText }>
+                                  	{ route.rightText || 'Right Button' }
+                                </Text>
+                              </TouchableHighlight> )
+  },
+  Title(route, navigator, index, navState) {
+    return <Text style={ styles.title }>MY APP TITLE</Text>
   }
+};
 
 
 class githubNoteTaker extends Component {
@@ -51,24 +60,66 @@ class githubNoteTaker extends Component {
 
   render() {
     return (
+      // <Navigator
+      //  initialRoute={{title: 'Github Notetaker'}}
+      //  renderScene={this.renderScene.bind(this)}
+      //  navigationBar={
+      //    <Navigator.NavigationBar
+      //    routeMapper={ NavigationBarRouteMapper }
+      //    />
+      //  }
+      ///>
+
       <Navigator
-        initialRoute={{title: 'Github Notetaker'}}
-        renderScene={this.renderScene.bind(this)}
-        navigationBar={
-          <Navigator.NavigationBar
-          routeMapper={ NavigationBarRouteMapper }
-          />
-        }
-      />
+    style={{flex:1}}
+    initialRoute={{title: 'Github Notetaker'}}
+    renderScene={ this.renderScene.bind(this) }
+    navigationBar={
+       <Navigator.NavigationBar
+         style={ styles.nav }
+         routeMapper={NavigationBarRouteMapper} />}
+/>
+
+
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#111',
-  },
+  mainContainer: {
+    	flex: 4,
+      flexDirection: 'column',
+      marginTop:100
+    },
+    leftNavButtonText: {
+    	fontSize: 18,
+      marginLeft:13,
+      marginTop:2
+    },
+    rightNavButtonText: {
+    	fontSize: 18,
+      marginRight:13,
+      marginTop:2
+    },
+    nav: {
+    	height: 60,
+      backgroundColor: '#efefef'
+    },
+    title: {
+    	marginTop:4,
+      fontSize:16
+    },
+    button: {
+    	height:60,
+      marginBottom:10,
+      backgroundColor: '#efefef',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    buttonText: {
+    	fontSize:18
+    },
 });
 
 AppRegistry.registerComponent('githubNoteTaker', () => githubNoteTaker);
